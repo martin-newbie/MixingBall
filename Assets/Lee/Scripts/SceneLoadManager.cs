30c0be283cbd9ec7b6bac13e7c1a7b450ec967eb
LeeJoon <leejun9334@gmail.com>
2022-12-17 22:36:58 +0900

    [#update] 시작버튼 눌렀을때 게임씬으로 진행

---
+++ Assets/Lee/Scripts/SceneLoadManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour
+{
+    [SerializeField] Animator canvasAnim;
+    
+
+    public void OnClickStartButton()
+    {
+        StartCoroutine(LoadingScene());
+    }
+
+    IEnumerator LoadingScene()
+    {
+        canvasAnim.SetTrigger("Start");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(1);
+    }
+}
